73b491fa766d81741b962cb51818d513a6b9809f
desit-dev <desit.dev@gmail.com>
2023-01-18 11:11:40 -0600

    mando subida de dropzone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dropzone_v1';
+
+  files: File[] = [];  
+
+  constructor(private http: HttpClient) { }  
+
+  onSelect(event:any) {  
+    console.log(event);  
+    this.files.push(...event.addedFiles);  
+
+    const formData = new FormData();  
+  
+    for (var i = 0; i < this.files.length; i++) {   
+      formData.append("type", "2");  
+      formData.append("belongs", "1");  
+      formData.append("file", this.files[i]);  
+    }  
+    
+    this.http.post('http://localhost:7800/resources', formData)  
+    .subscribe(res => {  
+      console.log(res);  
+      alert('Uploaded Successfully.'); 
+      this.files=[];  
+    })  
+}  
+
+onRemove(event:any) {  
+    console.log(event);  
+    this.files.splice(this.files.indexOf(event));  
+}  
 }
